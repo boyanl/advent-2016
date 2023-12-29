6f07d9bd04ce7fdb607aa6a001ec3211431edc10
Boyan Lazov <boyan.lazov@gmail.com>
2023-12-29 10:39:00 +0200

    Add heuristic that simplifies the input
    The heuristic is based on the observation that for each(generator, chip)
    pair on the 1st floor the time to solve increases by 12; and for each
    pair on the 2nd floor the time increases by 8 (so in general 4
    additional steps per pair per floor .. however for the 3rd floor this
    is not quite so simple - in general it is true that additional pairs
    cost 4 there, but for small values there are some anomalies .. so avoid
    optimizing that). So we can remove those pairs from the problem and
    just add the time to the result.

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,6 +1,7 @@
 use std::{
-    collections::{HashMap, HashSet, VecDeque},
-    fmt::format,
+    cmp::Reverse,
+    collections::{BinaryHeap, HashMap, HashSet, VecDeque},
+    fmt::Binary,
     io::stdin,
 };
 
@@ -26,12 +27,32 @@ fn add_item(f: FloorItems, item: Item) -> FloorItems {
     f | mask
 }
 
+fn contains_item(f: FloorItems, item: Item) -> bool {
+    add_item(f, item) == f
+}
+
+fn add_items(f: FloorItems, items: &[Item]) -> FloorItems {
+    let mut res = f;
+    for item in items {
+        res = add_item(res, *item);
+    }
+    res
+}
+
 fn remove_item(f: FloorItems, item: Item) -> FloorItems {
     let mask = match item {
         Item::Microchip(i) => 1 << i,
         Item::Generator(i) => 1 << (8 + i),
     };
     f & !mask
+}
+
+fn remove_items(f: FloorItems, items: &[Item]) -> FloorItems {
+    let mut res = f;
+    for item in items {
+        res = remove_item(res, *item);
+    }
+    res
 }
 
 fn items_count(f: FloorItems) -> u32 {
@@ -86,8 +107,14 @@ fn print_state(f: Floors, name_reverse_mapping: &HashMap<i32, String>) {
         let items_str = items(floor_items)
             .iter()
             .map(|item| match item {
-                Item::Generator(i) => format!("{} generator", name_reverse_mapping[i]),
-                Item::Microchip(i) => format!("{}-compatible microchip", name_reverse_mapping[i]),
+                Item::Generator(i) => format!(
+                    "{} generator",
+                    name_reverse_mapping.get(i).unwrap_or(&i.to_string())
+                ),
+                Item::Microchip(i) => format!(
+                    "{}-compatible microchip",
+                    name_reverse_mapping.get(i).unwrap_or(&i.to_string())
+                ),
             })
             .join(", ");
         println!("{floor}: {}", items_str);
@@ -250,46 +277,65 @@ fn next_states(floor: i32, floors: Floors) -> Vec<(i32, Floors)> {
     result
 }
 
-fn remaining_heuristic(items: Floors) -> i32 {
-    let mut result = 0;
-
-    let mut current_floor_items = 0;
-    for i in 0..3 {
-        let f = get_floor(items, i);
-        let items_cnt = items_count(f);
-        current_floor_items += items_cnt;
-
-        let pairs = current_floor_items / 2 + if current_floor_items % 2 == 0 { 0 } else { 1 };
-        result += if pairs <= 1 {
-            pairs
-        } else {
-            pairs + (pairs - 1)
-        };
+fn pairs(floor: FloorItems) -> Vec<(Item, Item)> {
+    let mut result = Vec::new();
+    for item in items(floor) {
+        match item {
+            Item::Generator(i) => {
+                if contains_item(floor, Item::Microchip(i)) {
+                    result.push((item, Item::Microchip(i)));
+                }
+            }
+            Item::Microchip(_) => {}
+        }
     }
-    result as i32
+    result
 }
 
-fn steps_required(items_state: Floors) -> i32 {
-    let mut q = VecDeque::new();
-    let start = (0, 0, items_state);
-    q.push_back(start);
+const optimize_heuristic: bool = true;
+
+fn steps_required(items_state: Floors, start_floor: i32) -> i32 {
+    let mut q = BinaryHeap::new();
+    let start = (Reverse(0), (start_floor, items_state));
+    q.push(start);
 
     let mut visited = HashSet::new();
-    visited.insert((0, items_state));
-
-    let items_cnt = total_items_count(items_state);
 
     while !q.is_empty() {
-        let (time, floor, floors_state) = q.pop_front().unwrap();
-        if floor == 3 && items_count(get_floor(floors_state, floor as usize)) == items_cnt {
+        let (time_rev, (floor, mut floors_state)) = q.pop().unwrap();
+        let mut time = time_rev.0;
+
+        if visited.contains(&(floor, floors_state)) {
+            continue;
+        }
+        visited.insert((floor, floors_state));
+
+        if floor == 3
+            && items_count(get_floor(floors_state, floor as usize))
+                == total_items_count(floors_state)
+        {
             return time;
         }
 
-        for next in next_states(floor, floors_state) {
-            if !visited.contains(&next) {
-                q.push_back((time + 1, next.0, next.1));
-                visited.insert(next);
+        if optimize_heuristic {
+            for (target_floor, &cost) in [12, 8].iter().enumerate() {
+                if floor == target_floor as i32 {
+                    let mut floor_items = get_floor(floors_state, target_floor);
+                    let pairs = pairs(floor_items);
+
+                    if pairs.len() > 1 {
+                        for &(item1, item2) in &pairs[1..] {
+                            floor_items = remove_items(floor_items, &[item1, item2]);
+                        }
+
+                        floors_state = set_floor(floors_state, target_floor, floor_items);
+                        time += (pairs.len() as i32 - 1) * cost;
+                    }
+                }
             }
+        }
+        for next in next_states(floor, floors_state) {
+            q.push((Reverse(time + 1), next));
         }
     }
 
@@ -299,7 +345,7 @@ fn steps_required(items_state: Floors) -> i32 {
 fn part_one() {
     let (start_state, _) = read_input();
 
-    let result = steps_required(start_state);
+    let result = steps_required(start_state, 0);
     println!("{result}");
 }
 
@@ -309,18 +355,35 @@ fn part_two() {
 
     // add "elerium" and "dilithium" generator + chip by hand
     let mut new_floor_0 = get_floor(start_state, 0);
-    new_floor_0 = add_item(
-        add_item(new_floor_0, Item::Generator(m + 1)),
-        Item::Microchip(m + 1),
-    );
-    new_floor_0 = add_item(
-        add_item(new_floor_0, Item::Generator(m + 2)),
-        Item::Microchip(m + 2),
+    new_floor_0 = add_items(
+        new_floor_0,
+        &[
+            Item::Generator(m + 1),
+            Item::Microchip(m + 1),
+            Item::Generator(m + 2),
+            Item::Microchip(m + 2),
+        ],
     );
     start_state = set_floor(start_state, 0, new_floor_0);
 
-    let result = steps_required(start_state);
+    let result = steps_required(start_state, 0);
     println!("{result}");
+}
+
+fn test_stuff() {
+    for floor in 0..3 {
+        let mut items = 0;
+        for pairs_cnt in 1..=7 {
+            items = add_items(
+                items,
+                &[Item::Microchip(pairs_cnt), Item::Generator(pairs_cnt)],
+            );
+            println!(
+                "{pairs_cnt} pairs, start floor = {floor}, result = {}",
+                steps_required(set_floor(0, floor as usize, items), floor as i32)
+            );
+        }
+    }
 }
 
 fn main() {
